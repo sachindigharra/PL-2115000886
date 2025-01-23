f872679e86d61c5557741ef71fec98296f5ac90e
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-01-24 02:03:39 +0530

    .....

--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("Testing of Git Repository Push & branching...");
-        System.out.println("Main branch");
-    }
-}
